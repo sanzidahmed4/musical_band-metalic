565a518f0faabe74dfce52123bc472db83198ddc
Sanzid Ahmed <mdsanjidahmed3@gmail.com>
2024-11-14 18:35:44 +0600

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+      const faqItem = question.parentElement;
+      faqItem.classList.toggle('active');
+    });
+  });
+  
